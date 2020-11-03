88ca84b2aa10faac3d69aeddd91fc19d04f91608
Lorenzo Garcia DallAgnol <lorenzogarciaa@outlook.com>
2020-11-03 04:51:38 -0300

    alterando chamada de index route

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -5,14 +5,11 @@ import { StoreRoutes } from './store/store.routes';
 import express from 'express';
 const routes = express.Router();
 
-const indexRoutes = [
-    routes.get('/', (req, res) => {
-        return res.status(200).send({
-            message: 'API em Node.js para desafio'
-        })
+routes.get('/', async (req, res) => {
+    return res.status(200).send({
+        message: 'API em Node.js para desafio'
     })
-]
-routes.use(indexRoutes)
+})
 routes.use(UserRoutes);
 routes.use(StoreRoutes);
 export default routes;
